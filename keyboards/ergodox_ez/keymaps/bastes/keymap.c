9cb3af76482b8ee8dbb491c31b1bfda246e35a2e
Michel Belleville <michel.belleville@gmail.com>
2019-07-09 17:47:30 +0200

    volume up/down should be windows compatible

--- keyboards/ergodox_ez/keymaps/bastes/keymap.c
+++ keyboards/ergodox_ez/keymaps/bastes/keymap.c
@@ -128,11 +128,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     KC_NO,   KC_NO,        KC_NO,
                                   KC_BTN1, KC_BTN2,      KC_BTN3,
   // right hand
-    KC_NO,   KC_NO,   KC_NO,       KC_NO,     KC_NO, KC_NO, KC_NO,
-  KC_PGUP,   KC_NO,   KC_NO,       KC_NO,     KC_NO, KC_NO, KC_NO,
-             KC_NO,   KC_NO,     KC_MPLY,     KC_NO, KC_NO, KC_NO,
-  KC_PGDN,   KC_NO,   KC_NO,     KC_MPRV,   KC_MNXT, KC_NO, KC_NO,
-                    KC_MUTE, KC__VOLDOWN, KC__VOLUP, KC_NO, KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+  KC_PGUP,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+             KC_NO,   KC_NO, KC_MPLY,   KC_NO, KC_NO, KC_NO,
+  KC_PGDN,   KC_NO,   KC_NO, KC_MPRV, KC_MNXT, KC_NO, KC_NO,
+                    KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,
     KC_NO,   KC_NO,
     KC_NO,   KC_NO,   KC_NO,
     KC_NO, KC_WBAK, KC_WFWD
