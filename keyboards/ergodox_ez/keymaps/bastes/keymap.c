0459fab34f5740b7086375bf429f1111062159e2
Michel Belleville <michel.belleville@lifen.fr>
2020-03-24 19:07:19 +0100

    feat(): one-shot L1 and, removed useless td and inner right column |&

--- keyboards/ergodox_ez/keymaps/bastes/keymap.c
+++ keyboards/ergodox_ez/keymaps/bastes/keymap.c
@@ -33,9 +33,7 @@ enum tapdance_keys {
   TD_SCREEN_UP = 0,
   TD_SCREEN_DOWN,
   TD_PREV_TAB,
-  TD_NEXT_TAB,
-  TD_UP_PGUP,
-  TD_DOWN_PGDOWN
+  TD_NEXT_TAB
 };
 
 
@@ -135,14 +133,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  TD  |           |  TD  |------+------+------+------+------+--------|
  * | ( / LS |Z/LCtl| X/Alt|   C  |   V  |   B  | ScDn |           | PTab |   N  |   M  |   ,  | ./Alt|//RCtl| ) / RS |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  | :L3  |      | Left | Right|                                       |  Up  | Down |      | :L3  | ~L1  |
+ *   | :L1  | :L3  |      | Left | Right|                                       |  Up  | Down |      | :L3  | :L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |  Up  | LGui |       | Home | PgUp |
  *                                 ,------|------|------|       |------+------+------.
  *                                 | Left | Down | Right|       | End  |PgDown| PScr |
  *                                 |------|------|------|       |------|------|------|
- *                                 | Space|Bckspc| Del  |       | ~L1  | Tab  |Enter |
+ *                                 | Space|Bckspc| Del  |       | :L1  | Tab  |Enter |
  *                                 `--------------------'       `--------------------'
  */
 [BASE] = LAYOUT_ergodox_80(
@@ -151,19 +149,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,   TD(TD_SCREEN_UP),
        KC_COLN,         KC_A,         KC_S,    KC_D,    KC_F,    KC_G,
        KC_LSPO, LCTL_T(KC_Z), LALT_T(KC_X),    KC_C,    KC_V,    KC_B, TD(TD_SCREEN_DOWN),
-         TT(1),       OSL(3),        KC_NO, KC_LEFT, KC_RGHT,
+        OSL(1),       OSL(3),        KC_NO, KC_LEFT, KC_RGHT,
                                                                 KC_UP,            KC_LGUI,
                                                      KC_LEFT, KC_DOWN,           KC_RIGHT,
                                                       KC_SPC, KC_BSPC,             KC_DEL,
   // right hand
-           KC_ESC,      KC_6,           KC_7,               KC_8,           KC_9,            KC_0, KC_MINS,
-  TD(TD_NEXT_TAB),      KC_Y,           KC_U,               KC_I,           KC_O,            KC_P, KC_BSLS,
-                        KC_H,           KC_J,               KC_K,           KC_L,   LT(2,KC_SCLN), KC_QUOT,
-  TD(TD_PREV_TAB),      KC_N,           KC_M,            KC_COMM, LALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_RSPC,
-                              TD(TD_UP_PGUP), TD(TD_DOWN_PGDOWN),          KC_NO,          OSL(3),   TT(1),
+           KC_ESC,      KC_6,    KC_7,    KC_8,           KC_9,            KC_0, KC_MINS,
+  TD(TD_NEXT_TAB),      KC_Y,    KC_U,    KC_I,           KC_O,            KC_P, KC_BSLS,
+                        KC_H,    KC_J,    KC_K,           KC_L,   LT(2,KC_SCLN), KC_QUOT,
+  TD(TD_PREV_TAB),      KC_N,    KC_M, KC_COMM, LALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_RSPC,
+                                KC_UP, KC_DOWN,          KC_NO,          OSL(3),  OSL(1),
           KC_HOME,   KC_PGUP,
-           KC_END, KC_PGDOWN,        KC_PSCR,
-            TT(1),    KC_TAB,         KC_ENT
+           KC_END, KC_PGDOWN, KC_PSCR,
+           OSL(1),    KC_TAB,  KC_ENT
 ),
 
 /* Keymap 1: Symbol Layer
@@ -171,10 +169,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | NLck |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   ?  |   |  |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |        |   !  |   @  |   {  |   }  |   ?  |      |           |      |   |  |   7  |   8  |   9  |   *  |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |--------+------+------+------+------+------|   &  |           |      |------+------+------+------+------+--------|
+ * |        |   #  |   $  |   (  |   )  |   `  |------|           |------|   &  |   4  |   5  |   6  |   +  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | CapLck |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |      |   1  |   2  |   3  |   /  | CapLck |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  vv  |      |      |   <  |   >  |                                       |   0  |   ,  |   .  |   =  |  vv  |
@@ -190,17 +188,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox_80(
   // left hand
    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_TRNS,
-X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES, KC_PIPE,
+X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES,   KC_NO,
    KC_NO, KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN,  KC_GRV,
- KC_CLCK, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_AMPR,
+ KC_CLCK, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   KC_NO,
  KC_TRNS,   KC_NO, KC_TRNS,   KC_LT,   KC_GT,
-                                             KC_TRNS, KC_TRNS,
-                                    KC_TRNS, KC_TRNS, KC_TRNS,
-                                    KC_TRNS, KC_TRNS, KC_TRNS,
+                                              KC_TRNS, KC_TRNS,
+                                     KC_TRNS, KC_TRNS, KC_TRNS,
+                                     KC_TRNS, KC_TRNS, KC_TRNS,
   // right hand
   KC_NLCK,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,
-    KC_NO,   KC_UP,   KC_P7,   KC_P8,  KC_P9, KC_PAST,  KC_F12,
-           KC_DOWN,   KC_P4,   KC_P5,  KC_P6, KC_PPLS,   KC_NO,
+    KC_NO, KC_PIPE,   KC_P7,   KC_P8,  KC_P9, KC_PAST,  KC_F12,
+           KC_AMPR,   KC_P4,   KC_P5,  KC_P6, KC_PPLS,   KC_NO,
     KC_NO,   KC_NO,   KC_P1,   KC_P2,  KC_P3, KC_PSLS, KC_CLCK,
                       KC_P0, KC_COMM, KC_DOT,  KC_EQL, KC_TRNS,
   KC_TRNS, KC_TRNS,
@@ -306,12 +304,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
   // next tab
   [TD_NEXT_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_NO, LCTL(KC_PGDOWN)),
-
-  // up / pgup
-  [TD_UP_PGUP] = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_PGUP),
-
-  // down / pgdown
-  [TD_DOWN_PGDOWN] = ACTION_TAP_DANCE_DOUBLE(KC_DOWN, KC_PGDOWN)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
