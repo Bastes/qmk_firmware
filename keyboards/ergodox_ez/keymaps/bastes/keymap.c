db541f3519ebc3416ee765b9a5e143a47d728869
Michel Belleville <michel.belleville@lifen.fr>
2020-04-01 09:48:18 +0200

    better caps lock placement

--- keyboards/ergodox_ez/keymaps/bastes/keymap.c
+++ keyboards/ergodox_ez/keymaps/bastes/keymap.c
@@ -170,13 +170,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | PScr |           | NLck |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * | PrtScr |  F1  |  F2  |  F3  |  F4  |  F5  | CLck |           | NLck |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |        |   !  |   @  |   {  |   }  |   ?  |      |           |      |   |  |   7  |   8  |   9  |   *  |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   #  |   $  |   (  |   )  |   `  |------|           |------|   &  |   4  |   5  |   6  |   +  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CapLck |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |      |   1  |   2  |   3  |   /  | CapLck |
+ * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |      |   1  |   2  |   3  |   /  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  vv  |      |      |   <  |   >  |                                       |   0  |   ,  |   .  |   =  |  vv  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -190,10 +190,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox_80(
   // left hand
-   KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_PSCR,
+ KC_PSCR,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_CLCK,
 X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES,   KC_NO,
    KC_NO, KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN,  KC_GRV,
- KC_CLCK, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   KC_NO,
+   KC_NO, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   KC_NO,
  KC_TRNS,   KC_NO, KC_TRNS,   KC_LT,   KC_GT,
                                               KC_TRNS, KC_TRNS,
                                      KC_TRNS, KC_TRNS, KC_TRNS,
@@ -202,7 +202,7 @@ X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES,   KC_NO,
   KC_NLCK,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,
     KC_NO, KC_PIPE,   KC_P7,   KC_P8,  KC_P9, KC_PAST,  KC_F12,
            KC_AMPR,   KC_P4,   KC_P5,  KC_P6, KC_PPLS,   KC_NO,
-    KC_NO,   KC_NO,   KC_P1,   KC_P2,  KC_P3, KC_PSLS, KC_CLCK,
+    KC_NO,   KC_NO,   KC_P1,   KC_P2,  KC_P3, KC_PSLS,   KC_NO,
                       KC_P0, KC_COMM, KC_DOT,  KC_EQL, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,   KC_NO,
