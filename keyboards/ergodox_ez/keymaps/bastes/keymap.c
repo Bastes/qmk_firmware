02aaba25ea96799657472c70b48ddf38c929eaa6
Michel Belleville <michel.belleville@lifen.fr>
2020-05-27 15:32:05 +0200

    back to simpler shift and correcting the fps layer

--- keyboards/ergodox_ez/keymaps/bastes/keymap.c
+++ keyboards/ergodox_ez/keymaps/bastes/keymap.c
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| ScUp |           | NTab |------+------+------+------+------+--------|
  * |    :   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  ' "   |
  * |--------+------+------+------+------+------|  TD  |           |  TD  |------+------+------+------+------+--------|
- * | ( / LS |Z/LCtl| X/Alt| C/L1 | V/L3 |   B  | ScDn |           | PTab |   N  | M/L3 | ,/L1 | ./Alt|//RCtl| ) / RS |
+ * | LShift |Z/LCtl| X/Alt| C/L1 | V/L3 |   B  | ScDn |           | PTab |   N  | M/L3 | ,/L1 | ./Alt|//RCtl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | :L1  | :L3  |  TL5 | Left | Right|                                       |  Up  | Down |      | :L3  | :L1  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,        KC_3,        KC_4,      KC_5,             KC_ESC,
         KC_EQL,         KC_Q,         KC_W,        KC_E,        KC_R,      KC_T,   TD(TD_SCREEN_UP),
        KC_COLN,         KC_A,         KC_S,        KC_D,        KC_F,      KC_G,
-       KC_LSPO, LCTL_T(KC_Z), LALT_T(KC_X), LT(1, KC_C), LT(3, KC_V),      KC_B, TD(TD_SCREEN_DOWN),
+       KC_LSFT, LCTL_T(KC_Z), LALT_T(KC_X), LT(1, KC_C), LT(3, KC_V),      KC_B, TD(TD_SCREEN_DOWN),
         OSL(1),       OSL(3),        TG(4),     KC_LEFT,     KC_RGHT,
                                                                         KC_PGUP,            KC_HOME,
                                                               KC_ESC, KC_PGDOWN,             KC_END,
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_ESC,    KC_6,        KC_7,           KC_8,           KC_9,            KC_0, KC_MINS,
   TD(TD_NEXT_TAB),    KC_Y,        KC_U,           KC_I,           KC_O,            KC_P, KC_BSLS,
                       KC_H,        KC_J,           KC_K,           KC_L,   LT(2,KC_SCLN), KC_QUOT,
-  TD(TD_PREV_TAB),    KC_N, LT(3, KC_M), LT(1, KC_COMM), LALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_RSPC,
+  TD(TD_PREV_TAB),    KC_N, LT(3, KC_M), LT(1, KC_COMM), LALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_RSFT,
                                   KC_UP,        KC_DOWN,          KC_NO,          OSL(3),  OSL(1),
           KC_LGUI,   KC_UP,
           KC_LEFT, KC_DOWN,    KC_RIGHT,
@@ -321,7 +321,7 @@ X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES,   KC_NO,
    KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,   KC_ESC,
    KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,
   KC_CLCK,  KC_A,  KC_S,    KC_D,    KC_F,    KC_G,
-  KC_SLSH,  KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,
+  KC_LSFT,  KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,
     KC_NO, KC_NO, TG(4), KC_LEFT, KC_RGHT,
                                              KC_UP,  KC_LGUI,
                                   KC_LEFT, KC_DOWN, KC_RIGHT,
@@ -330,7 +330,7 @@ X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES,   KC_NO,
    KC_ESC,    KC_6,     KC_7,    KC_8,   KC_9,    KC_0, KC_MINS,
     KC_NO,    KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSLS,
               KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT,
-    KC_NO,    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
+    KC_NO,    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                        KC_UP, KC_DOWN,  KC_NO,   KC_NO,   KC_NO,
   KC_LGUI,   KC_UP,
   KC_LEFT, KC_DOWN, KC_RIGHT,
