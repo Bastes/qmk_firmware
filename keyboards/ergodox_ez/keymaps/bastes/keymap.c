97f86b90eb8349ce11ca8a352af9aac704cfa5c0
Michel Belleville <michel.belleville@gmail.com>
2020-07-27 17:03:29 +0200

    shifts and minor cleanup on the symbols layer

--- keyboards/ergodox_ez/keymaps/bastes/keymap.c
+++ keyboards/ergodox_ez/keymaps/bastes/keymap.c
@@ -177,9 +177,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   #  |   $  |   (  |   )  |   `  |------|           |------|   &  |   4  |   5  |   6  |   +  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   =  |   1  |   2  |   3  |   /  |        |
+ * | LShift |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   =  |   1  |   2  |   3  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  vv  |      |      |   <  |   >  |                                       |   0  |   ,  |   .  |      |  vv  |
+ *   |      |      |      |      |      |                                       |   0  |   ,  |   .  |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |  vv  |  vv  |       |  vv  |  vv  |
@@ -194,17 +194,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_PSCR,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_CLCK,
 X(THINK), KC_EXLM,   KC_AT, KC_LCBR, KC_RCBR, KC_QUES,   KC_NO,
    KC_NO, KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN,  KC_GRV,
-   KC_NO, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   KC_NO,
- KC_TRNS,   KC_NO, KC_TRNS,   KC_LT,   KC_GT,
-                                              KC_TRNS, KC_TRNS,
-                                     KC_TRNS, KC_TRNS, KC_TRNS,
-                                     KC_TRNS, KC_TRNS, KC_TRNS,
+ KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   KC_NO,
+   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                                              KC_TRNS, KC_TRNS,
+                                                     KC_TRNS, KC_TRNS, KC_TRNS,
+                                                     KC_TRNS, KC_TRNS, KC_TRNS,
   // right hand
   KC_NLCK,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,
     KC_NO, KC_PIPE,   KC_P7,   KC_P8,  KC_P9, KC_PAST,  KC_F12,
            KC_AMPR,   KC_P4,   KC_P5,  KC_P6, KC_PPLS,   KC_NO,
-    KC_NO,  KC_EQL,   KC_P1,   KC_P2,  KC_P3, KC_PSLS,   KC_NO,
-                      KC_P0, KC_COMM, KC_DOT,   KC_NO, KC_TRNS,
+    KC_NO,  KC_EQL,   KC_P1,   KC_P2,  KC_P3, KC_PSLS, KC_TRNS,
+                      KC_P0, KC_COMM, KC_DOT,   KC_NO,   KC_NO,
   KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,   KC_NO,
   KC_TRNS, KC_TRNS, KC_TRNS
