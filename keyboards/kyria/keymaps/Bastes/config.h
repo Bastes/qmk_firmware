feef1e8616694df14cd692a8021ceb60ba5b9071
Michel Belleville <michel@42network.org>
2021-03-24 14:55:05 +0100

    using the right side as master (port near busted on the left)

--- keyboards/kyria/keymaps/Bastes/config.h
+++ keyboards/kyria/keymaps/Bastes/config.h
@@ -48,3 +48,6 @@
 
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_LED_MAP { 0, 1, 2, 9, 8, 7, 4, 3, 5, 6, 19, 18, 17, 10, 11, 12, 15, 16, 14, 13 }
+
+// Using the right side as the master
+#define MASTER_RIGHT
