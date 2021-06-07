57712b5125889b18b2771e1f531a4caeed76b4a0
Michel Belleville <michel@42network.org>
2021-06-07 15:22:04 +0200

    fixup! fix post-rebase ; disabled unused features

--- keyboards/kyria/keymaps/Bastes/rules.mk
+++ keyboards/kyria/keymaps/Bastes/rules.mk
@@ -1,7 +1,7 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enables keyboard RGB underglow
-TAP_DANCE_ENABLE = no      # Enables custom tap dance actions
-EXTRAFLAGS += -flto        # Limit Time Optimization shoud save space (?)
-SPACE_CADET_ENABLE = no    # saving space on unused features
-GRAVE_ESC_ENABLE = no      # saving space on unused features
+OLED_DRIVER_ENABLE   = yes   # Enables the use of OLED displays
+ENCODER_ENABLE       = yes   # Enables the use of one or more encoders
+RGBLIGHT_ENABLE      = yes   # Enables keyboard RGB underglow
+TAP_DANCE_ENABLE     = no    # Enables custom tap dance actions
+EXTRAFLAGS          += -flto # Limit Time Optimization shoud save space (?)
+SPACE_CADET_ENABLE   = no    # saving space on unused features
+GRAVE_ESC_ENABLE     = no    # saving space on unused features
