f8fc9cf7773c32deb7b0e669f8b0a8e3432728c9
Michel Belleville <michel.belleville@gmail.com>
2023-03-23 15:31:19 +0100

    attempting to resucitate my old firmware

--- keyboards/splitkb/kyria/keymaps/Bastes/rules.mk
+++ keyboards/splitkb/kyria/keymaps/Bastes/rules.mk
@@ -3,3 +3,12 @@ OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+UNICODEMAP_ENABLE = yes    # Enable unicode
+MOUSEKEY_ENABLE = yes      # Enable mouse keys
+
+EXTRAFLAGS += -flto        # Limit Time Optimization shoud save space (?)
+CONSOLE_ENABLE = no        # saving space on unused features
+COMMAND_ENABLE = no        # saving space on unused features
+SPACE_CADET_ENABLE = no    # saving space on unused features
+GRAVE_ESC_ENABLE = no      # saving space on unused features
+MAGIC_ENABLE = no          # saving space on unused features
